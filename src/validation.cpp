c59ef86d1ad1a8f5a9ff07489c95c413bccea75c
bluejaytodd <jkseo98@hanmail.net>
2019-04-06 11:27:14 +0900

    pool node chainwork temporary solution

--- src/validation.cpp
+++ src/validation.cpp
@@ -1371,11 +1371,13 @@ void CheckForkWarningConditions() {
         chainActive.Height() - pindexBestForkTip->nHeight >= 72)
         pindexBestForkTip = nullptr;
 
-    if (pindexBestForkTip ||
-        (pindexBestInvalid &&
-         pindexBestInvalid->nChainWork >
-             chainActive.Tip()->nChainWork +
-                 (GetBlockProof(*chainActive.Tip()) * 6))) {
+    //temporal setting after fork : bluejaytodd
+    //if (pindexBestForkTip ||
+    //    (pindexBestInvalid &&
+    //     pindexBestInvalid->nChainWork >
+    //         chainActive.Tip()->nChainWork +
+    //             (GetBlockProof(*chainActive.Tip()) * 6))) {
+    if (pindexBestForkTip ){ 
         if (!GetfLargeWorkForkFound() && pindexBestForkBase) {
             std::string warning =
                 std::string("'Warning: Large-work fork detected, forking after "
